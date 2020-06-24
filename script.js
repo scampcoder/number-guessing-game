b5fdb860d7a71870972a2e6d8916e96064dcd9fe
Victoria Kee <victoriakee95@gmail.com>
2020-06-24 10:21:09 -0500

    Write main function for checking user guess

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ let resetButton;
 
 /*call focus method on guessField*/
 guessField.focus();
+
+/*main game function*/
+function checkGuess() {
+  let userGuess = Number(guessField.value); /*user's input*/
+  if(guessCount === 1) { /*if this is user's first guess*/
+    guesses.textContent = 'Previous guesses: ';
+  }
+  guesses.textContent += userGuess + ' '; /*user's guesses*/
+  guesses.style.backgroundColor = 'blue'; /*when user guesses are appended, give blue background*/
+  if(userGuess === randomNumber) { /*if user guesses correctly*/
+    lastResult.textContent = "Congratulations! You got it right!"; /*Game won message (with green background)*/
+    lastResult.style.backgroundColor = 'green';
+    lowOrHi.textContent = ''; /*clear low or high values*/
+    setGameOver(); /*run game over*/
+  }else if (guessCount === 10) { /*if user guesses 10 times the game is over*/
+    lastResult.textContent = "***GAME OVER***";
+    lowOrHi.textContent = '';
+    setGameOver();
+  }else {
+    lastResult.textContent = 'Wrong!'; /*if guesses are under 10 and incorrect*/
+    lastResult.style.backgroundColor = "red";
+    if(userGuess < randomNumber) { /*alert user if guess too low*/
+      lowOrHi.textContent = "Last guess too low!";
+    }else if(userGuess > randomNumber) { /*alert user if guess too high*/
+      lowOrHi.textContent = "Last guess too high!";
+    }
+  }
+  guessCount++; /*increment guessCount each guess*/
+  guessField.value = ''; /*clear input field*/
+  guessField.focus(); /*run focus method on input field*/
+}
