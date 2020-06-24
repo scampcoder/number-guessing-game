e8d8b8f58e4b4de1709afec66af15e870e1b066f
Victoria Kee <victoriakee95@gmail.com>
2020-06-24 10:20:12 -0500

    Run focus on input field

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*delcare/assign main vars*/
 let randomNumber = Math.floor(Math.random() * 500) + 1;
 const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
@@ -6,3 +7,6 @@ const guessSubmit = document.querySelector('.guessSubmit');
 const guessField = document.querySelector('.guessField');
 let guessCount = 1;
 let resetButton;
+
+/*call focus method on guessField*/
+guessField.focus();
